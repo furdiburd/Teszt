1c1e79675a227f3181584a1cc4e268620f14d01f
YOLOOOO <example@test.microsoft>
2026-01-29 13:34:05 +0100

    Masodik osztaly kesz

---
+++ Masodik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+	internal class Masodik
+	{
+		List<Adat> list;
+		public Masodik()
+		{
+			list = new List<Adat>();
+		}
+	}
+}
